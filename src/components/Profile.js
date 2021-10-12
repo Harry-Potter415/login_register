cda37bc2d512b8cc6aa95dca107c42e4268c8842
Harry <olivedreamdev@gmail.com>
2021-10-12 19:18:12 +0900

    update store

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,110 +1,38 @@
-import React, { useState, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import React from "react";
+import { Redirect } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
+const Profile = () => {
+  const { user: currentUser } = useSelector((state) => state.auth);
 
-import { login } from "../actions/auth";
+  if (!currentUser) {
+    return <Redirect to="/login" />;
+  }
 
-const required = (value) => {
-    if (!value) {
-        return (
-            <div className = "alertalert-danger" role="alert">
-                This field is required!
-            </div>
-        );
-    }
+  return (
+    <div className="container">
+      <header className="jumbotron">
+        <h3>
+          <strong>{currentUser.username}</strong> Profile
+        </h3>
+      </header>
+      <p>
+        <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
+        {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
+      </p>
+      <p>
+        <strong>Id:</strong> {currentUser.id}
+      </p>
+      <p>
+        <strong>Email:</strong> {currentUser.email}
+      </p>
+      <strong>Authorities:</strong>
+      <ul>
+        {currentUser.roles &&
+          currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
+      </ul>
+    </div>
+  );
 };
 
-const Login = (props) => {
-    const form = useRef();
-    const checkBtn = useRef();
-    
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-
-    const { isLoggedIn } = useSelector(state => state.auth);
-    const { message } = useSelector(state => state.message);
-    
-    const dispatch = useDispatch();
-    
-    const onChangeUsername = (e) => {
-        const username = e.target.value;
-        setUsername(username);
-    };
-
-    const onChangePassword = (e) => {
-        const password = e.target.value;
-        setPassword(password);
-    };
-
-    const handleLogin = (e) => {
-        e.preventDefault();
-
-        setLoading(true);
-
-        form.current.validateAll();
-
-        if (checkBtn.current.context._errors.length === 0) {
-            dispatch(login(username, password))
-            .then(() => {
-                props.history.push("/profile");
-                window.location.reload();
-            })
-            .catch(()=> {
-                setLoading(false);
-            });
-        } else {
-            setLoading(false);
-        }
-    };
-
-    if (isLoggedIn) {
-        return <Redirect to="/profile" />
-    }
-
-    return (
-        <div className="col-md-12">
-            <div className="card card-container">
-                <img
-                src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-                alt="profile-img"
-                className="profile-img-card"
-                />
-
-                <Form onSubmit={handleLogin} ref={form}>
-                    <div className="form-group">
-                        <label htmlFor="username">Username</label>
-                        <Input type="text" className="form-control" name="username" value={username} onChange={onChangeUsername} validations={[required]} />
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <Input type="password" className="form-control" name="password" value={password} onChange={onChangePassword} validations={[required]} />
-                    </div>
-
-                    <div className="form-group">
-                        <button className="btn btn-primary btn-block" disabled={loading}>
-                            {loading && (
-                                <span className="spinner-border spinner-border-sm"></span>
-                            )}
-                            <span>Login</span>
-                        </button>
-                    </div>
-
-                    {message && (
-                        <div className="form-group">
-                            <div className="alert alert-danger" role="alert">
-                                {message}
-                            </div>
-                        </div>
-                    )}
-                    <CheckButton style={{ display: "none" }} ref={checkBtn} />
-                </Form>
-            </div>
-        </div>
-    );
-};
+export default Profile;
